477592d28e7f0f2723e11773e7f7994ed5bb878b
DiaaEldinHassan <diaahassan777@gmail.com>
2023-12-03 19:41:22 +0200

    Third commit

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ var city="";
 var limit="";
 var lon="";
 var lat="";
-const weather=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`
+var celsius=0;
+var atmo="";
+const weather=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -23,34 +25,41 @@ app.get("/form",(req,res)=>{
 });
 app.get("/weather",async(req,res)=>{
     res.render("weather.ejs");
-    try {
-        // const response=await axios.get("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=e2d2712c5e552740dd2230ac66165b22");
-        // console.log(response);
-    } catch (error) {
-        console.log(error);
-    }
+    // console.log(fullTime);
 });
 app.post("/submit",async(req,res)=>{
     console.log(req.body);
-    res.render("weather.ejs");
     try {
         city=req.body.city;
         limit=req.body.limit;
         country=req.body.country;
+        
         console.log(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${API}`);
         const response=await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${API}`);
+        console.log(response.data[0].country);
         for (let index = 0; index < limit; index++) {
             if (response.data[index].country==country) {
              lat=response.data[index].lat;
-             lon=response.data[index].lon;  
+             lon=response.data[index].lon;
             }   
         }
-        const result=await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`)
-        console.log(result);
+        const result=await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`);
+        celsius=temp(result.data.main.temp);
+        atmo=result.data.weather[0].main;
+        console.log(result.data.weather[0].main);
+        console.log(result.data);
+        res.render("weather.ejs",{weather:celsius,atmo:atmo});
     } catch (error) {
-        console.log(error);
+        console.log(error.status);
+        res.render("weather.ejs",{weather:error.status});
     }
 });
+function temp(weather)
+{
+    var kelvin=-273.15;
+    var cel=weather+kelvin;
+    return Math.round(cel);
+}
 app.listen(port,()=>{
     console.log(`Connected successfully to port ${port}`);
 });
