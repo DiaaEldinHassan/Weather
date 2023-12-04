d6187b6410d167c3ac4a4bcd87efb0a67e1eaaa9
DiaaEldinHassan <diaahassan777@gmail.com>
2023-12-04 21:12:56 +0200

    Fifth commit

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ var lon="";
 var lat="";
 var celsius=0;
 var atmo="";
-const weather=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`;
+const ss='https://sunrise-sunset-times.p.rapidapi.com/getSunriseAndSunset';
+var hourper="";
+var hourper1st="";
+var hourper2nd="";
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -26,17 +30,15 @@ app.get("/form",(req,res)=>{
 });
 app.get("/weather",async(req,res)=>{
     res.render("weather.ejs");
+
 });
 app.post("/submit",async(req,res)=>{
-    console.log(req.body);
+
     try {
         city=req.body.city;
         limit=req.body.limit;
         country=req.body.country;
-        
-        console.log(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${API}`);
         const response=await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${API}`);
-        console.log(response.data[0].country);
         for (let index = 0; index < limit; index++) {
             if (response.data[index].country==country) {
              lat=response.data[index].lat;
@@ -44,11 +46,13 @@ app.post("/submit",async(req,res)=>{
             }   
         }
         const result=await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`);
+        hourper=dayNight();
+        hourper1st=hourper.slice(0,1);
+        hourper2nd=hourper.slice(1,3);
         celsius=temp(result.data.main.temp);
         atmo=result.data.weather[0].main;
-        res.render("weather.ejs",{weather:celsius,atmo:atmo});
+        res.render("weather.ejs",{weather:celsius,atmo:atmo,city:city,country:country,hour:hourper1st,period:hourper2nd});
     } catch (error) {
-
         res.render("weather.ejs",{weather:error.status});
     }
 });
@@ -57,6 +61,37 @@ function temp(weather)
     var kelvin=-273.15;
     var cel=weather+kelvin;
     return Math.round(cel);
+}
+function dayNight()
+{
+    var date = new Date();
+    var day=date.getDay();
+    // Get current hour
+    var hour = date.getHours();
+    // Get current minute
+    var minute = date.getMinutes();
+    // Get current second
+    var second = date.getSeconds();
+   
+    // Variable to store AM / PM
+    var period = "";
+   
+    // Assigning AM / PM according to the current hour
+    if (hour >= 12) {
+    period = "PM";
+    } else {
+    period = "AM";
+    }
+   
+    // Converting the hour in 12-hour format
+    if (hour == 0) {
+    hour = 12;
+    } else {
+    if (hour > 12) {
+    hour = hour - 12;
+    }
+    }
+    return(hour+period);
 }
 app.listen(port,()=>{
     console.log(`Connected successfully to port ${port}`);
--- public/styles/js/script.js
+++ public/styles/js/script.js
@@ -1,8 +1,9 @@
 function Time() {
 
     // Creating object of the Date class
+    var week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     var date = new Date();
-   
+    var day=date.getDay();
     // Get current hour
     var hour = date.getHours();
     // Get current minute
@@ -34,13 +35,21 @@ function Time() {
     hour = update(hour);
     minute = update(minute);
     second = update(second);
-   
+   $(".day").text(week[day]);
     // Adding time elements to the div
-    $(".time").text(hour+":"+minute+":"+second); 
+    $(".time").text(" "+hour+":"+minute+":"+second+" "+period); 
+    $(".time").css("color", "lime");
    
     // Set Timer to 1 sec (1000 ms)
-    setTimeout(Time, 1000);
+    setTimeout(()=>{
+        Time();
+    }, 1000);
+    setTimeout(() => {
+        $(".time").css("color", "black");
+    }, 500);
    }
+
+  
    
     // Function to update time elements if they are less than 10
     // Append 0 before time elements if they are less than 10
@@ -52,5 +61,5 @@ function Time() {
     return t;
     }
    }
-   
+ 
    Time();
